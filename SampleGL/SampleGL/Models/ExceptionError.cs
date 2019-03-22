371a209ccf338858a4319979ffe404bf66285929
Sakthi <narayanansakthi1992@gmail.com>
2019-03-22 15:14:46 +0530

    Entityframework and unity

--- SampleGL/SampleGL/Models/ExceptionError.cs
+++ SampleGL/SampleGL/Models/ExceptionError.cs
@@ -5,7 +5,18 @@ using System.Web;
 
 namespace SampleGL.Models
 {
-    public class ExceptionError
+    public class ExceptionError : A
     {
     }
+
+    public abstract class A
+    {
+        LinkedList<string> str = new LinkedList<string>();
+    }
+
+    public abstract class B
+    {
+
+    }
+
 }
